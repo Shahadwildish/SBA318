bd555d02b586fb2733671f3483e269807a333a65
shahadwildish <shayalobeidi@gmail.com>
2024-08-02 11:53:58 -0400

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { randomInt } = require('crypto');
 const app = express();
 const port = 3000;
-let nextId = 4;
+let nextId = randomInt(5000);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join('.\\', 'views'));
